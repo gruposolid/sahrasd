77dc57d1a2d4ca049cbf045d34891012926db013
falquinho <lucas.falcao.nb@gmail.com>
2017-02-10 09:58:54 -0300

    Base Input System created

---
+++ SahrasdUnity/Assets/InputSystem/PlayerOneJoystick.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneJoystick : VirtualJoystick
+{
+    public Vector2 LeftAnalogueData()
+    {
+        Vector2 data = Vector2.zero;
+        data.x = Input.GetRawAxis("P1LeftHorizontal");
+        data.y = Input.GetRawAxis("P1LeftVertical");
+        return data;
+    }
+    
+    public Vector2 RightAnalogueData()
+    {
+        Vector2 data = Vector2.zero;
+        data.x = Input.GetRawAxis("P1RightHorizontal");
+        data.y = Input.GetRawAxis("P1RightVertical");
+        return data;
+    }
+    
+    public  bool IsStartPressed()
+    {
+        return Input.GetButton("P1Start");
+    }
+    
+    public  bool IsSelectPressed()
+    {
+        return Input.GetButton("P1Select");
+    }
+    
+    // Face Buttons
+    public  bool IsFaceUpPressed()
+    {
+        return Input.GetButton("P1FaceUp");
+    }
+    
+    public  bool IsFaceDownressed()
+    {
+        return Input.GetButton("P1FaceDown");
+    }
+    
+    public  bool IsFaceLeftPressed()
+    {
+        return Input.GetButton("P1FaceLeft");
+    }
+    
+    public  bool IsFaceRightPressed()
+    {
+        return Input.GetButton("P1FaceRight");
+    }
+    
+    
+    // Arrow Buttons
+    public  bool IsArrowUpPressed()
+    {
+        return Input.GetButton("P1ArrowUp");
+    }
+    
+    public  bool IsArrowDownressed()
+    {
+        return Input.GetButton("P1ArrowDown");
+    }
+    
+    public  bool IsArrowLeftPressed()
+    {
+        return Input.GetButton("P1ArrowLeft");
+    }
+    
+    public  bool IsArrowRightPressed()
+    {
+        return Input.GetButton("P1ArrowRight");
+    }
+    
+    
+    // Left Shoulder Buttons
+    public  bool IsLeftShoulderPressed()
+    {
+        return Input.GetButton("P1LeftShoulder");
+    }
+    
+    public  bool IsLeftTriggerPressed()
+    {
+        return Input.GetButton("P1LeftTrigger");
+    }
+    
+    
+    // Right Shoulder Buttons
+    public  bool IsRightShoulderPressed()
+    {
+        return Input.GetButton("P1RightShoulder");
+    }
+    
+    public  bool IsRightTriggerPressed()
+    {
+        return Input.GetButton("P1RightTrigger");
+    }
+}
---
+++ SahrasdUnity/Assets/InputSystem/VirtualJoystick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class that represents a Joystick (gamepad, controller ...)
+// Concrete subclasses need simply to do the mapping
+// VirtualButton-->Unity Input Button  
+public abstract class VirtualJoystick  
+{
+    public abstract Vector2 LeftAnalogueData();
+    public abstract Vector2 RightAnalogueData();
+    
+    public abstract bool IsStartPressed();
+    public abstract bool IsSelectPressed();
+    
+    public abstract bool IsFaceUpPressed();
+    public abstract bool IsFaceDownressed();
+    public abstract bool IsFaceLeftPressed();
+    public abstract bool IsFaceRightPressed();
+    
+    public abstract bool IsArrowUpPressed();
+    public abstract bool IsArrowDownressed();
+    public abstract bool IsArrowLeftPressed();
+    public abstract bool IsArrowRightPressed();
+    
+    public abstract bool IsLeftShoulderPressed();
+    public abstract bool IsLeftTriggerPressed();
+    
+    public abstract bool IsRightShoulderPressed();
+    public abstract bool IsRightTriggerPressed();
+}
